798f5c516736ec09856720afdf5148e6265f7535
tsaqyf <muhammad.t.mumtaz@gmail.com>
2026-09-14 20:43:32 +0700

    complete controller

--- src/main/java/com/example/tracker/controller/OrdersController.java
+++ src/main/java/com/example/tracker/controller/OrdersController.java
@@ -1,16 +1,18 @@
 package com.example.tracker.controller;
 
+import com.example.tracker.dto.ActiveStepViewResponse;
 import com.example.tracker.dto.CreateOrdersRequest;
 import com.example.tracker.entity.Orders;
+import com.example.tracker.entity.OrdersRoute;
 import com.example.tracker.service.OrdersService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
@@ -23,4 +25,30 @@ public class OrdersController {
         return ResponseEntity.status(HttpStatus.CREATED).body(orders);
     }
 
+    @PatchMapping("/{ordersId}/ordersRoute/{ordersRouteId}/complete")
+    public ResponseEntity<OrdersRoute> completeRoute(
+            @PathVariable UUID ordersId,
+            @PathVariable UUID ordersRouteId,
+            @RequestHeader("X-Station-Code") String stationsCode,
+            @RequestHeader("X-Users-Id") UUID usersId
+    ){
+        OrdersRoute ordersRoute = ordersService.FinishRoute(ordersId,ordersRouteId,stationsCode,usersId);
+        return ResponseEntity.ok(ordersRoute);
+    }
+
+    @PatchMapping("/{ordersId}/cancel")
+    public ResponseEntity<void> cancel(
+            @PathVariable UUID ordersId,
+            @RequestHeader("X-Station-Code") String stationsCode
+    ){
+        ordersService.cancelRoute(ordersId,stationsCode);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/ordersRoute/active")
+    public ResponseEntity<List<ActiveStepViewResponse>> getActiveViewStations(
+            @RequestHeader("X-Stations-Code") String stationsCode
+    ){
+        return ResponseEntity.ok(ordersService.getActiveSteps(stationsCode));
+    }
 }
